250d2eea63cdae842bc6617151a05e89b4d69ed1
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-12-16 20:58:42 -0300

    feat: add a util to strings

---
+++ src/main/java/jsvariedades/sales/util/Strings.java
@@ -0,0 +1,33 @@
+package jsvariedades.sales.util;
+
+public class Strings {
+
+    public static final String SPACE = " ";
+    public static final String COLON = ":";
+    public static final String COMMA = ",";
+    public static final String DOT = ".";
+    public static final String DASH = "-";
+    public static final String UNDERSCORE = "_";
+    public static final String EMPTY = "";
+    public static final String NEW_LINE = "\n";
+    public static final String LINE_BREAK = "<br>";
+    public static final String SLASH = "/";
+    public static final String PERCENTAGE = "%";
+    public static final String SEMICOLON = ";";
+    public static final String PIPE = "|";
+    public static final String AT = "@";
+    public static final String CARET = "^";
+    public static final String DOLLAR = "$";
+    public static final String ASTERISK = "*";
+    public static final String BACK_SLASH = "\\";
+    public static final String SHARP = "#";
+    public static final String AND = "&";
+    public static final String QUESTION_MARK = "?";
+    public static final String EXCLAMATION_MARK = "!";
+    public static final String OPEN_BRACE = "{";
+    public static final String CLOSE_BRACE = "}";
+    public static final String OPEN_CLOSE_BRACES = OPEN_BRACE + CLOSE_BRACE;
+
+    private Strings() {
+    }
+}
