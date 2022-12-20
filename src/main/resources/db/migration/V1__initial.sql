350ac8c0e2b0af3dd569d38ad2090ad857a8de19
Elyson Vinicius <elysonvinicius@outlook.com>
2022-12-20 14:59:06 -0300

    feat[feature/JS001]: create table User

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -6,3 +6,16 @@ CREATE TABLE Role (
      PRIMARY KEY (id)
 );
 
+CREATE TABLE User(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    username varchar(100) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    is_active boolean NOT NULL,
+    created_date timestamp,
+    updated_date timestamp,
+    img_link varchar(255) NOT NULL,
+    role_id int NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES Role(id)
+)
+
